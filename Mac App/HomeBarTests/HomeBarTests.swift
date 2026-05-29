23a322da1436515b627deca030f9d6ed389ef505
Pax Willoughby <kilter_waspish0l@icloud.com>
2026-05-29 00:33:06 -0700

    Add xcode project

---
+++ Mac App/HomeBarTests/HomeBarTests.swift
@@ -0,0 +1,19 @@
+//
+//  HomeBarTests.swift
+//  HomeBarTests
+//
+//  Created by Pax Willoughby on 5/29/26.
+//
+
+import Testing
+@testable import HomeBar
+
+struct HomeBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
